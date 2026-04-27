ace70a8fdb98dc83c0ca8ff6fb966c5b1989bade
Priyanshu Saxena <priyanshu.p.saxena07@gmail.com>
2026-04-27 07:55:31 -0700

    feat: practice break and continue with even number loop

---
+++ src/loops/BreakAndContinue.java
@@ -0,0 +1,23 @@
+package loops;
+
+public class BreakAndContinue {
+    public static void main(String[] args) {
+
+        for (int i = 2; i <= 30; i += 2) {
+            if (i == 15 || i == 20)continue;
+            if (i == 28)break;
+            System.out.println(i);
+
+        }
+
+
+
+        }
+
+
+    }
+
+
+ //     for (int i = 2; i <= 20; i += 2) {
+//            System.out.println(i);
+//        }
