03bd0b9cf5f5fda68e43d11fda5a02ac41de5c36
Priyanshu Saxena <priyanshu.p.saxena07@gmail.com>
2026-04-29 01:17:00 -0700

    Find maximum element in array

--- src/Arrays/PracticeArray.java
+++ src/Arrays/PracticeArray.java
@@ -3,21 +3,22 @@ package Arrays;
 public class PracticeArray {
     public static void main(String[] args) {
 
-        int[] arr = {4, 2, 7, 1};
+        int[] arr =  {10, 25, 3, 99, 8};
 
-        int min = arr[0];
+         int max = arr[0];
 
-        for (int i = 1; i < arr.length; i++) {
-            if (arr[i] < min) {
-                min = arr[i];
-            }
-
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > max)
+                max = arr[i];
 
         }
-        System.out.println("Min: " + min);
+        System.out.println("Max: " + max);
+
+        }
+
         }
 
 
 
-    }
+
 
