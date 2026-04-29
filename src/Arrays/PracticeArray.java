b85eac1f3ee13584a14e9b8615e771842b752543
Priyanshu Saxena <priyanshu.p.saxena07@gmail.com>
2026-04-29 02:57:37 -0700

    Find second largest element in array

--- src/Arrays/PracticeArray.java
+++ src/Arrays/PracticeArray.java
@@ -3,16 +3,23 @@ package Arrays;
 public class PracticeArray {
     public static void main(String[] args) {
 
-        int[] arr =  {10, 25, 3, 99, 8};
+        int[] arr = {10, 25, 3, 99, 8};
 
-         int max = arr[0];
+        int largest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
 
         for (int i = 0; i < arr.length; i++) {
-            if (arr[i] > max)
-                max = arr[i];
 
+            if (arr[i] > largest) {
+                secondLargest = largest;
+                largest = arr[i];
+            }
+            else if (arr[i] > secondLargest && arr[i] != largest) {
+                secondLargest = arr[i];
+            }
         }
-        System.out.println("Max: " + max);
+
+        System.out.println("Second Largest: " + secondLargest);
 
         }
 
