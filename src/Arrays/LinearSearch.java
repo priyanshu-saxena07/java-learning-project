249de2677b6dda12c006d1964f4f24767cc35c4f
Priyanshu Saxena <priyanshu.p.saxena07@gmail.com>
2026-04-30 01:55:50 -0700

    Implement linear search to find element index in array

--- src/Arrays/LinearSearch.java
+++ src/Arrays/LinearSearch.java
@@ -2,30 +2,21 @@ package Arrays;
 
 public class LinearSearch {
     public static void main(String[] args) {
-       int[] arr = {1, 4, 6, 8};
-       int target = 4;
 
-       int index = -1;
+        int[] arr = {3, 7, 2, 9, 5};
+        int target = 9;
+
+        int index = -1;
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] == target) {
                 index = i;
                 break;
             }
+
         }
-        System.out.println(index);
+        System.out.println("Index: " + index);
         }
 
     }
 
 
-    //           int[] arr = {1, 3, 5, 7};
-//        int target = 5;
-//
-//        int index = -1;
-//        for (int i = 0; i < arr.length; i++) {
-//            if (arr[i] == target) {
-//                index = i;
-//                break;
-//            }
-//        }
-//        System.out.println(index);
