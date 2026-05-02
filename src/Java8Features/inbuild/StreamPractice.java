ff081766ffeeb9c760f2229e015756d27071fc7c
Priyanshu Saxena <priyanshu.p.saxena07@gmail.com>
2026-05-02 01:42:33 -0700

    feat: Implemented Stream API map operation to double each number in list

--- src/Java8Features/inbuild/StreamPractice.java
+++ src/Java8Features/inbuild/StreamPractice.java
@@ -6,10 +6,10 @@ import java.util.List;
 public class StreamPractice {
     public static void main(String[] args) {
 
-        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8);
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
 
         numbers.stream()
-                .filter(n -> n % 2 != 0)
+                .map(n -> n * 2)
                 .forEach(System.out::println);
 
 
