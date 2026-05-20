6fd20e6b6ec49c1d918bc9d201472e5810434805
Priyanshu Saxena <priyanshu.p.saxena07@gmail.com>
2026-05-20 00:05:40 -0700

    feat: add simple error handling example in java streams

--- src/Java8Features/intermediate/ErrorHandlingStream.java
+++ src/Java8Features/intermediate/ErrorHandlingStream.java
@@ -1,18 +1,22 @@
 package Java8Features.intermediate;
 
 import java.util.List;
+import java.util.Arrays;
 
 public class ErrorHandlingStream {
     public static void main(String[] args) {
-        List<String> books = List.of("Java", "Python", "C++");
-        books.stream()
-                .forEach(b -> {
+
+        List<String> inputs = Arrays.asList("10", "abc", "30");
+
+        inputs.stream()
+                .map(str -> {
                     try {
-                        if (b.equals("Python"))
-                            throw new RuntimeException("Invalid book");
-                    } catch (Exception e) {
-                        e.printStackTrace();
+                        return Integer.parseInt(str);
+                    } catch (NumberFormatException e) {
+                        System.out.println("Found bad data: " + str + ". Using fallback -1");
+                        return -1;
                     }
-                });
+                })
+                .forEach(System.out::println);
     }
 }
