6e7830498fa46f61f182569e1c8c47e1974cbb9a
Priyanshu Saxena <priyanshu.p.saxena07@gmail.com>
2026-05-21 10:49:44 -0700

    feat: LeetCode 35 Search Insert Position solution using Binary Search

--- src/DSA/Arrays/SearchInsertPosition.java
+++ src/DSA/Arrays/SearchInsertPosition.java
@@ -22,7 +22,7 @@ public class SearchInsertPosition {
 
     public static void main(String[] args) {
         int[] nums = {1, 3, 5, 6};
-        int target = 2;
+        int target = 5;
 
         int result =  searchInsertn(nums, target);
         System.out.println("Result: " + result);
