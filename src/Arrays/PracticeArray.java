f10fca3cd1be915f417182e988569c548d728958
Priyanshu Saxena <priyanshu.p.saxena07@gmail.com>
2026-06-01 12:03:48 +0530

    feat: add logic to calculate positive and negative numbers sum in array

--- src/Arrays/PracticeArray.java
+++ src/Arrays/PracticeArray.java
@@ -2,21 +2,37 @@ package Arrays;
 
 public class PracticeArray {
     public static void main(String[] args) {
+        int[] numbers = {12, -7, 5, -3, 14, -1, 0, -8, 9};
 
-        int[] arr = {1,2,3,4,5,6,7};
-
-        int sum = 0;
-
-        for (int num : arr) {
-            sum += num;
+        int posSum = 0;
+        int negSum = 0;
+        for (int num : numbers) {
+            if (num > 0) {
+                 posSum += num;
+            } else if (num < 0) {
+                 negSum += num;
+            }
         }
-        double average = (double) sum / arr.length;
-        System.out.println("Average = " + average);
+
+        System.out.println("PosSum: " + posSum);
+        System.out.println("NegSum: " + negSum);
+
+
+
+
 
     }
 }
 
-
+//       int[] arr = {1,2,3,4,5,6,7};
+//
+//        int sum = 0;
+//
+//        for (int num : arr) {
+//            sum += num;
+//        }
+//        double average = (double) sum / arr.length;
+//        System.out.println("Average = " + average);
 
 
 
