b88ca434beb8b455eed6f17020771a4da553a6d4
Priyanshu Saxena <priyanshu.p.saxena07@gmail.com>
2026-06-02 11:08:55 +0530

    feat: add array element counter for zeros and ones

--- src/Arrays/PracticeArray.java
+++ src/Arrays/PracticeArray.java
@@ -2,37 +2,39 @@ package Arrays;
 
 public class PracticeArray {
     public static void main(String[] args) {
-        int[] numbers = {12, -7, 5, -3, 14, -1, 0, -8, 9};
 
-        int posSum = 0;
-        int negSum = 0;
-        for (int num : numbers) {
-            if (num > 0) {
-                 posSum += num;
-            } else if (num < 0) {
-                 negSum += num;
+    int[] arr = {1,0,1,1,0,1,1};
+    int zeroCount = 0;
+    int oneCount = 0;
+        for (int num : arr) {
+            if (num == 0) {
+                zeroCount++;
+            } else {
+                oneCount++;
             }
+
         }
-
-        System.out.println("PosSum: " + posSum);
-        System.out.println("NegSum: " + negSum);
-
-
-
-
+        System.out.println("zeroCount: " + zeroCount);
+        System.out.println("oneCount: " +   oneCount);
 
     }
 }
 
-//       int[] arr = {1,2,3,4,5,6,7};
+
+ //     int[] numbers = {12, -7, 5, -3, 14, -1, 0, -8, 9};
 //
-//        int sum = 0;
-//
-//        for (int num : arr) {
-//            sum += num;
+//        int posSum = 0;
+//        int negSum = 0;
+//        for (int num : numbers) {
+//            if (num > 0) {
+//                 posSum += num;
+//            } else if (num < 0) {
+//                 negSum += num;
+//            }
 //        }
-//        double average = (double) sum / arr.length;
-//        System.out.println("Average = " + average);
+//
+//        System.out.println("PosSum: " + posSum);
+//        System.out.println("NegSum: " + negSum);
 
 
 
