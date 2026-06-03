2dbcdcba8fdb673d48f5f9d7055363259ec78e00
Priyanshu Saxena <priyanshu.p.saxena07@gmail.com>
2026-06-03 10:54:20 +0530

    fix: update array sorting check to handle duplicate elements

--- src/Arrays/PracticeArray.java
+++ src/Arrays/PracticeArray.java
@@ -1,40 +1,38 @@
 package Arrays;
 
 public class PracticeArray {
+
+    public static int getUnsortedElement(int[] arr) {
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (arr[i+1] < arr[i]) {
+                return arr[i+1];
+            }
+        }
+        return -1;
+    }
+
     public static void main(String[] args) {
 
-    int[] arr = {1,0,1,1,0,1,1};
-    int zeroCount = 0;
-    int oneCount = 0;
-        for (int num : arr) {
-            if (num == 0) {
-                zeroCount++;
-            } else {
-                oneCount++;
-            }
-
-        }
-        System.out.println("zeroCount: " + zeroCount);
-        System.out.println("oneCount: " +   oneCount);
-
+        int[] arr = {1, 2, 2, 5, 4, 9};
+        System.out.println(getUnsortedElement(arr)); // Output: 4
     }
 }
 
 
- //     int[] numbers = {12, -7, 5, -3, 14, -1, 0, -8, 9};
-//
-//        int posSum = 0;
-//        int negSum = 0;
-//        for (int num : numbers) {
-//            if (num > 0) {
-//                 posSum += num;
-//            } else if (num < 0) {
-//                 negSum += num;
+
+
+//       int[] arr = {1,0,1,1,0,1,1};
+//    int zeroCount = 0;
+//    int oneCount = 0;
+//        for (int num : arr) {
+//            if (num == 0) {
+//                zeroCount++;
+//            } else {
+//                oneCount++;
 //            }
-//        }
 //
-//        System.out.println("PosSum: " + posSum);
-//        System.out.println("NegSum: " + negSum);
-
-
+//        }
+//        System.out.println("zeroCount: " + zeroCount);
+//        System.out.println("oneCount: " +   oneCount);
 
