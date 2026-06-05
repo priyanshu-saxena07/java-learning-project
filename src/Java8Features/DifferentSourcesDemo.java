0f5825cbc0958739710ebaa83b0da396c7ec234f
Priyanshu Saxena <priyanshu.p.saxena07@gmail.com>
2026-06-05 22:40:27 +0530

    feat: add Stream API demo for List and Array sources

--- src/Java8Features/DifferentSourcesDemo.java
+++ src/Java8Features/DifferentSourcesDemo.java
@@ -1,8 +1,5 @@
 package Java8Features;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.IntStream;
@@ -10,19 +7,13 @@ import java.util.stream.Stream;
 
 public class DifferentSourcesDemo {
     public static void main(String[] args) {
-
-        List<Integer> numbers = Arrays.asList(1, 2, 3, 4 , 5, 6, 7, 8);
+        List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6, 7, 8);
         Stream<Integer> integerStream = numbers.stream();
         integerStream.forEach(System.out::println);
 
-        int[] numberArray = {1,2,3,4,};
-         IntStream arrayStream = Arrays.stream(numberArray);
+
+        int[] numberArray = {1, 2, 3, 4};
+        IntStream arrayStream = Arrays.stream(numberArray);
         arrayStream.forEach(System.out::println);
-
-
-
-
-
-
     }
 }
