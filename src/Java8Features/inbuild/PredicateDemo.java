a88d6fd1cb5b9306e870ffad90526cabdf08fbf1
Priyanshu Saxena <priyanshu.p.saxena07@gmail.com>
2026-06-19 21:50:15 +0530

    refactor: simplify salary predicate logic and fix naming

--- src/Java8Features/inbuild/PredicateDemo.java
+++ src/Java8Features/inbuild/PredicateDemo.java
@@ -1,15 +1,21 @@
 package Java8Features.inbuild;
 
-import java.util.function.BiPredicate;
 import java.util.function.Predicate;
 
 public class PredicateDemo {
     public static void main(String[] args) {
-        Predicate<Integer> isEven = n -> n % 2 == 0;
-        System.out.println(isEven.test(10));
-        System.out.println(isEven.test(101));
 
-        BiPredicate<Integer, Integer> isSumEven = (a, b) -> (a + b) % 2 ==0;
-        System.out.println(isSumEven.test(10,201));
+        Predicate<Double> HighSalary = salary -> salary > 85000;
+        System.out.println(HighSalary.test(50000.0));
+        System.out.println(HighSalary.test(90000.0));
+
     }
 }
+
+
+//      Predicate<Integer> isEven = n -> n % 2 == 0;
+//        System.out.println(isEven.test(10));
+//        System.out.println(isEven.test(20));
+//
+//        BiPredicate<Integer, Integer> isSumEven = (a, b) -> (a + b) % 2 ==0;
+//        System.out.println(isSumEven.test(10,201));
