9d76ef2c7626c05ad4e304ca2c11555ed8737548
Priyanshu Saxena <priyanshu.p.saxena07@gmail.com>
2026-06-20 18:26:12 +0530

    feat: implement function chaining using compose method in java 8

--- src/Java8Features/inbuild/FunctionInterfaceDemo.java
+++ src/Java8Features/inbuild/FunctionInterfaceDemo.java
@@ -5,13 +5,14 @@ import java.util.function.Function;
 public class FunctionInterfaceDemo {
 
     public static Function<Integer, Integer> addFunction = (a) -> a + 3;
-    public static Function<Integer, Integer> substraceFunction = (a) -> a - 3;
-
+    public static Function<Integer, Integer> multiplyFunction = (a) -> a * 2;
 
     public static void main(String[] args) {
-        System.out.println(addFunction.apply(10));
-        System.out.println(substraceFunction.apply(10));
+        // It will do multiply first: (10 * 2) = 20, then addition: (20 + 3) = 23
+        Function<Integer, Integer> composedFunction = addFunction.compose(multiplyFunction);
 
+        System.out.println("Result of compose: " + composedFunction.apply(10));
+        // Output: 23
 
     }
 }
