3ec4153984e60df05d8f35435ac117870c7eaae2
Priyanshu Saxena <priyanshu.p.saxena07@gmail.com>
2026-07-01 13:08:17 +0530

    feat: add example to find duplicate elements using Java Streams

--- src/Java8Features/FirstStreamDemo.java
+++ src/Java8Features/FirstStreamDemo.java
@@ -1,26 +1,22 @@
 package Java8Features;
 
-import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
-import java.util.function.Predicate;
-import java.util.stream.Stream;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class FirstStreamDemo {
     public static void main(String[] args) {
 
-        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8);
+        List<Integer> list = Arrays.asList(1, 2, 3, 2, 4, 3, 5);
 
-        numbers.stream()
-                .filter(n -> n % 2 != 0)
-                .forEach(n -> System.out.println(n));
+        // Filter and collect into a list
 
-        List<Integer> numbers2 = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8);
-
-            numbers.stream()
-                    .filter(n -> n % 2 ==0)
-                    .forEach(n -> System.out.println(n));
-
+        Set<Integer> duplicates = list.stream()
+                .filter(n -> Collections.frequency(list, n) > 1)
+                .collect(Collectors.toSet());
+        System.out.println(duplicates);
 
 
 
