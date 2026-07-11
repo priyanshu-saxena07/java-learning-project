1042cfcb975f8b4521598ed0dfbcd5c543778644
Priyanshu Saxena <priyanshu.p.saxena07@gmail.com>
2026-07-11 14:12:59 +0530

    feat: implement logic to find duplicate elements in an array using HashSet

--- src/Arrays/SolutionArrays.java
+++ src/Arrays/SolutionArrays.java
@@ -1,17 +1,36 @@
 package Arrays;
 
+import java.util.HashSet;
+import java.util.Set;
+
+@SuppressWarnings("ConstantConditions")
 public class SolutionArrays {
     public static void main(String[] args) {
 
-        int[] numbers = {1, 2, 3, 4, 5, 6};
+    int[] arr = {10,30,30,20,55,5,7,55,};
+    if (arr == null || arr.length == 0) {
+        System.out.println("Array is empty");
+        return;
+    }
 
-        for (int num : numbers) {
-            if (num % 2 == 0) {
-                int squared = num * num;
-                System.out.println(squared);
+        Set<Integer> uniqueNumber = new HashSet<>();
+        System.out.println("Duplicate Element");
+
+        for (int num: arr) {
+            if (!uniqueNumber.add(num)) {
+                System.out.println(num);
             }
         }
+
 
     }
 }
 
+//  int[] numbers = {1, 2, 3, 4, 5, 6};
+//
+//        for (int num : numbers) {
+//            if (num % 2 != 0) {
+//                int squared = num * num;
+//                System.out.println(squared);
+//            }
+//        }
