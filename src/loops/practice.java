62f0843945253d2469433f0b63ddb9406c5d8a47
Priyanshu Saxena <priyanshu.p.saxena07@gmail.com>
2026-07-23 23:14:27 +0530

    feat: add basic loop practice with continue statement

--- src/loops/practice.java
+++ src/loops/practice.java
@@ -2,13 +2,16 @@ package loops;
 
 public class practice {
     public static void main(String[] args) {
-        int rows = 4;
 
-        for (int i = 1; i <= rows; i++) {
-            for (int j = 1; j <= i; j++) {
-                System.out.print("* ");
+        int count = 0;
+        for (int i = 1; i <= 3; i ++) {
+            if (i == 2) {
+                continue;
             }
-            System.out.println();
+            count = count + i;
         }
+        System.out.println(count);
+
+
     }
 }
