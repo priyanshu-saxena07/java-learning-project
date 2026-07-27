bdb1199cac46dc3024bc54d27fbe575a135df5b1
Priyanshu Saxena <priyanshu.p.saxena07@gmail.com>
2026-07-27 17:23:12 +0530

    feat: implement even-index array printing

---
+++ src/Arrays/Array.java
@@ -0,0 +1,17 @@
+package Arrays;
+
+public class Array {
+    public static void main( String[] args) {
+
+           int[] arr = {1, 2, 3, 4, 5};
+
+        for (int i = 0; i < arr.length; i += 2) {
+            System.out.print(arr[i] + " ");
+
+        }
+        System.out.println();
+
+    }
+}
+
+
