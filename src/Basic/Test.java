911a91576762420efd224bd4442a9aca3421a4bd
Priyanshu Saxena <priyanshu.p.saxena07@gmail.com>
2026-07-31 21:51:16 +0530

    Practice: Implement Java program for pre/post increment and && operator

---
+++ src/Basic/Test.java
@@ -0,0 +1,11 @@
+package Basic;
+
+public class Test {
+    public static void main(String[] args) {
+       int a = 5;
+
+       if (a++ == 5 && ++a == 7) {
+           System.out.println(a);
+       }
+    }
+}
